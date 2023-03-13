48ba26b36493f328b9c8aedeb4893515bed33506
Dave Demers <ddemers@c2r6p3.42quebec.com>
2023-03-13 14:23:40 -0400

    abc

--- src/main/struct.h
+++ src/main/struct.h
@@ -6,7 +6,7 @@
 /*   By: ddemers <ddemers@student.42quebec.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 07:26:14 by ddemers           #+#    #+#             */
-/*   Updated: 2023/03/12 17:13:16 by ddemers          ###   ########.fr       */
+/*   Updated: 2023/03/13 13:16:43 by ddemers          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,17 @@
 // linked list of commands example listed in comments of how a node would look like
 typedef struct s_cmds
 {
-	char			**cmds; // {"cat" , NULL};
-	char			*path; // "bin/ls"; Parsing doesn't have to worry about this one
-	int				fd_in; //  Parsing doesn't have to worry about this one, for execution, store the fd for pipes
-	int				fd_out; //  Parsing doesn't have to worry about this one, for execution, store the fd for pipes
-	int				in_type; // Type of redirection, > < >> << |, have to come up with the way we want to do it
-	int				out_type; // Type of redirection, > < >> << |, have to come up with the way we want to do it
-	char			*infile; /*For < <<, store the name of the file. If < was the infile redirection, here an example and what it would store from the example. file1.txt < cat | wc. This would store file1.txt has a string */
-	char			*outfile; // Same thing from infile, but for the > >>
-	pid_t			pid; //child pid for execution only
-	struct s_cmds	*previous; // Point to previous node, function that create the node deal with it
-	struct s_cmds	*next; // Point to next node, function that create the node deal with it
+	char			**cmds;
+	char			*path;
+	int				fd_in;
+	int				fd_out;
+	int				in_type;
+	int				out_type;
+	char			*infile;
+	char			*outfile;
+	pid_t			pid;
+	struct s_cmds	*previous;
+	struct s_cmds	*next;
 }	t_cmds;
 
 /*
