ccd7656baf327b6466205faddc3ae7590fcf7e47
producks <davetrap.demers@gmail.com>
2023-04-06 15:34:35 -0400

    xd

---
+++ src/parsing/parsing_utils_2.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing_utils_2.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cperron <cperron@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/01 18:32:33 by cperron           #+#    #+#             */
+/*   Updated: 2023/04/03 19:06:18 by cperron          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "parsing.h"
+
+int	find_cmds(char **tokens, int i)
+{
+	while (is_redir_2(tokens[i]) == 1 && tokens[i + 2])
+		i +=2;
+	return (i);
+}
+
+int	find_pipe(char **tokens, int i)
+{	
+	while (tokens[i])
+	{
+		if (is_pipe(tokens[i]) == 1)
+			return(i);
+		i++;
+	}
+	return (0);
+}
